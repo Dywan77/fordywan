057d556ee2ab5722655c185472eb05efc21eefaf
Hydral <nathan.dacunha.nd@gmail.com>
2021-01-07 19:57:08 +0100

    ajout de lobjets musique

---
+++ Musique.h
@@ -0,0 +1,38 @@
+#ifndef __Musique_H__
+#define __Musique_H__
+
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+enum e_musique{
+	MUSIQUEMENU = 0,
+	MUSIQUEJEUX = 1
+};
+
+class Musique{
+
+	public:
+		Musique();
+		void playZone(int zone);
+		void stop()
+		{
+			m_music.stop();
+		}
+		void intro(){
+			m_music.setLoop(false);
+			m_music.openFromFile("song/debut.wav");
+			m_music.play();
+			sf::Clock tmpclock;
+			while(tmpclock.getElapsedTime().asMilliseconds() <= 2500);
+			m_music.stop();
+			m_music.setLoop(true);
+			std::cout << "HAHAHAHAH" << std::endl << std::endl;
+		}
+	private:
+		sf::Music m_music;
+};
+
+
+
+
+#endif
